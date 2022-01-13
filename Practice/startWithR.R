932328e4b81e0ef0710485767ebd2740ac9ca92a
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-01-13 14:13:24 +0530

    starting with R

---
+++ Practice/startWithR.R
@@ -0,0 +1,22 @@
+a = 4
+b = 5
+#print(a+b)
+print(paste('a:',a))
+print(paste('b:',b))
+print(paste0("Sum of 2 numbers: ",(a+b)))
+
+# paste inside print will add space between terms
+# where as print0 will not include any space by self
+
+x = "Hello World!"  # string
+x1 = 255            # integer
+x2 = 23.14          # float 
+
+# string print
+print(sprintf("%s is a string", x))
+
+# integer print
+print(sprintf("%d is integer", x1))
+
+# float print
+print(sprintf("%f is float", x2))

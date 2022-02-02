49a6b09f4b1376f6c5fc814f0e21146ca51cd2ba
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-02 21:40:52 +0530

    upto ques 2(II) rules made

--- Practical2.R
+++ Practical2.R
@@ -1,5 +1,5 @@
 #Perform the following preprocessing tasks on the dirty_iris dataset
-#i)Calculate the number and percentage of observations that are complete.
+
 #ii)Replace all the special values in data with NA.
 #iii)Define these rules in a separate text file and read them.
 #(Use editfile function in R (package editrules). 
@@ -12,7 +12,17 @@
 #iv)Determine how often each rule is broken (violatedEdits). Also summarize and plot the result.
 #v)Find outliers in sepal length using boxplot and boxplot.stats
 
-data <- read.table(file = "dirty_iris.csv",sep = ",", header = TRUE)
-head(data,10)
-summary(df)
-str(df)
+data<-read.table(file = 'dirty_iris.csv', sep=',', header = TRUE)
+print(head(data,10))
+print(summary(data))
+str(data)
+
+#i)Calculate the number and percentage of observations that are complete.
+complete_cases <- complete.cases(data)
+
+as.numeric(complete_cases)
+n_complete<-sum(complete_cases)
+print(n_complete)
+
+percent_complete<- 100*n_complete/nrow(data)
+print(paste("Cases in DIRTY_IRIS dataframe that are complete: ", percent_complete, "%"))

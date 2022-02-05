3e8c137cfda8314a7ad426666733e0fe2a87bb52
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-02-05 21:11:17 +0530

    start with practical 3

---
+++ Practical3.R
@@ -0,0 +1,3 @@
+#Load the data from wine dataset. 
+#Check whether all attributes are standardized or not (mean is 0 and standard deviation is 1). 
+#If not, standardize the attributes. Do the same with Iris dataset.

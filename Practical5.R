da2b7583b1ec677edc3e3532f3ab680fff701751
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-03-04 21:11:47 +0530

    decision tree for 75-25 and hold out method

--- Practical5.R
+++ Practical5.R
@@ -10,3 +10,47 @@
 #Compare the accuracy of the classifiers obtained.
 #5.3 Data is scaled to standard format.
 
+#DECISION TREE
+data(iris)
+print(summary(iris))
+print(head(iris,5))
+
+#part a
+#Hold-out Method
+
+library(caTools)
+set.seed(143)
+split<-sample.split(iris$Species,SplitRatio = 0.75)
+
+training_set = subset(iris,split==TRUE)
+testing_set = subset(iris,split==FALSE)
+
+#summary(training_set)
+#summary(testing_set)
+
+print(dim(training_set))
+print(dim(testing_set))
+
+library(rpart)
+#?rpart
+
+dtm<-rpart(Species~.,training_set,method = "class")
+#dtm #gives textual description
+#plot(dtm)
+#text(dtm) #adding text to plot
+
+#install.packages("rpart.plot")
+library(rpart.plot)
+
+rpart.plot(dtm)
+rpart.plot(dtm,type = 4, extra = 101)
+
+p<-predict(dtm,testing_set,type = "class")
+
+library(caret)
+
+#print(confusionMatrix(testing_set[,5],p))
+print(confusionMatrix(testing_set[,5],p)$table)
+print(confusionMatrix(testing_set[,5],p)$overall["Accuracy"]*100)
+
+

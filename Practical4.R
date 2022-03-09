a79a959a8ca6e8a07ef530e5f55e4584dc7b7285
g-4-gagan <gagankumar190814@keshav.du.ac.in>
2022-03-09 23:38:25 +0530

    Start with practical 4

---
+++ Practical4.R
@@ -0,0 +1,3 @@
+# Q4. Run Apriori algorithm to find frequent itemsets and association rules
+# 1.1 Use minimum support as 50% and minimum confidence as 75%
+# 1.2 Use minimum support as 60% and minimum confidence as 60 %

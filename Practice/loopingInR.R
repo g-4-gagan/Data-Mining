cb074883dd6f69438e72effaf5645051775945be
Gagan Kumar Soni <73790190+g-4-gagan@users.noreply.github.com>
2022-03-29 23:29:13 +0530

    looping in r programming

---
+++ Practice/loopingInR.R
@@ -0,0 +1,15 @@
+# using for loops
+for (val in 1: 5)
+{
+    # statements to be executed
+    print(val)
+}
+
+#iterating through items of a list
+items<-c("hello","world","good","morning")
+
+for (item in items)
+{
+    #using the item values inside loop
+    print(item)
+}
